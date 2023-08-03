973151ef52c98c524da11abfc16546327b79f5ec
JonathanCoene <coenejonathan@hotmail.com>
2023-08-03 12:48:10 +0200

    finisged exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,21 +1,20 @@
 const reverseString = function(string) {
-
     const convertArray = Array.from(string);
-    console.log(convertArray.length);
     console.log(convertArray);
 
-    let output = "";
+    let output = "output: ";
 
     for (let i = 0; i < convertArray.length; i++) {
         output += convertArray[convertArray.length-1-i];
-    }
-
-    return output;
-};
+    }  
+    
+return output;
+}
 
 console.log(reverseString("hello"));
 console.log(reverseString("hello there"));
 console.log(reverseString("123! abc!"));
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
