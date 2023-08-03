749d787abf497480643981d0f406b22d48686a2b
JonathanCoene <coenejonathan@hotmail.com>
2023-08-03 13:43:53 +0200

    exercise 04 - test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+let list = [1, 2, 3, 4];
 
-};
+const removeFromArray = function(array, value) {
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == value){
+            array.splice(i,1);
+        }
+    }
+    
+    return array;
+}
+
+console.log(removeFromArray(list, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
