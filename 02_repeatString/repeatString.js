1ca7ce750e94f510328878101fe21c68d7d5bcbe
JonathanCoene <coenejonathan@hotmail.com>
2023-08-03 09:47:50 +0200

    exercise 02 - first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+    return output;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
