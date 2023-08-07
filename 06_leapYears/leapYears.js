c71936ca39674b650ccb0e1765c0a0617b615990
JonathanCoene <coenejonathan@hotmail.com>
2023-08-07 16:34:27 +0200

    finished exercise 06 - test 2

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,15 @@
 const leapYears = function(year) {
     if (year % 4 == 0){
         return true;
+    } else {
+        return false;
     }
 
 };
 
 
 console.log(leapYears(1996));
-
+console.log(leapYears(1997));
 
 
 // Do not edit below this line
