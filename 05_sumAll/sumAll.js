619febd337592112ff16c06665d797b504d7eeec
JonathanCoene <coenejonathan@hotmail.com>
2023-08-07 16:01:04 +0200

    finished exercise 05 - test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(firstValue, secondValue) {
+    let output = 0;
 
+    for (let i = firstValue; i <= secondValue; i++) {
+        output += i;
+    }
+    return output;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
