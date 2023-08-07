1ba71207e70c09bf4f778ac2c892ba814d9aa72b
JonathanCoene <coenejonathan@hotmail.com>
2023-08-07 17:10:43 +0200

    finished exercise 07 - test 1 + 2 + 3

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * 5/9;
+  return convertedTemp.toFixed(1);
+
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(temp) {
 };
+
+
+
+
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
+
+
+
 
 // Do not edit below this line
 module.exports = {
