e66767b15cacb90d47c9d04d683ad69151f3d160
JonathanCoene <coenejonathan@hotmail.com>
2023-08-08 14:20:47 +0200

    finished exercise 09 - test 2 - 5

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,25 @@
 const palindromes = function (string) {
-    const array = string.split("");
-
-    for (let i = 0; i < array.length;) {
-        if (array[i] === array[array.length - 1 - i]) {
+    let input = string.toLowerCase();
+    const array = input.split("");
+    const filteredArray = array.filter(function(i) {return i !== "." && i !== "!" && i !== "," && i !== "?" && i !== " "});
+    
+    for (let i = 0; i < filteredArray.length;i++) {
+        if (filteredArray[i] === filteredArray[filteredArray.length - 1 - i]) {
             return "true";
+        } else {
+            return "false";
         }
     }
-
 };
 
 console.log(palindromes('racecar'));
-
+console.log(palindromes('racecar!'));
+console.log(palindromes('Racecar!'));
+console.log(palindromes('A car, a man, a maraca.'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+console.log(palindromes('ZZZZ car, a man, a maracaz.')); // werkt nog niet
+console.log(palindromes('rac3e3car')); // werkt nog niet
+console.log(palindromes('r3ace3car')); // werkt nog niet
 
 
 // Do not edit below this line
