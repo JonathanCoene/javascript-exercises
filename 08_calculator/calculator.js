514726043c391ff6e008bfc699553cadcef5c9bc
JonathanCoene <coenejonathan@hotmail.com>
2023-08-08 11:32:49 +0200

    finished exercise 08 - test 4 -10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,15 +6,14 @@ const add = function(valueOne, valueTwo) {
 const subtract = function(valueOne, valueTwo) {
   let output = valueOne - valueTwo;
   return output;
-	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
 const power = function() {
@@ -31,6 +30,14 @@ console.log(add(2, 2));
 console.log(add(2, 6));
 
 console.log(subtract(10, 4));
+
+console.log(sum([]));
+console.log(sum([7]));
+console.log(sum([7, 11]));
+console.log(sum([1, 3, 5, 7, 9]));
+
+console.log(multiply([2, 4]));
+console.log(multiply([2, 4, 6, 8, 10, 12, 14]));
 
 
 // Do not edit below this line
