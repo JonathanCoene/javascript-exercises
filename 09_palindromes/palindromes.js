46086feed73f2fb9f2dee38d32f9eaf94891aba4
JonathanCoene <coenejonathan@hotmail.com>
2023-08-08 13:50:05 +0200

    finished exercise 09 - test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const array = string.split("");
+
+    for (let i = 0; i < array.length;) {
+        if (array[i] === array[array.length - 1 - i]) {
+            return "true";
+        }
+    }
 
 };
+
+console.log(palindromes('racecar'));
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
